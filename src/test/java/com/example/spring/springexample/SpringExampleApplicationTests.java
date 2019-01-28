df744d682220b1a256537a8e4c913101f1331b8f
Ziem0 <andrzejewski.ziemo@gmail.com>
2019-01-28 22:30:31 +0100

    changes

--- src/test/java/com/example/spring/springexample/SpringExampleApplicationTests.java
+++ src/test/java/com/example/spring/springexample/SpringExampleApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.spring.springexample;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class SpringExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertEquals(1, 1);
 	}
 
 }
